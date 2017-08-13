5ff6dfc50b07acfddd00586b7786dfd99a661daa
Mali Michael <malimccalla@gmail.com>
2017-08-13 21:37:06 +0100

    Delete mutation and refetching data

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import { graphql, gql } from 'react-apollo';
+import { graphql, gql, compose } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSongs from '../queries/fetchSongs';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props
+      .mutate({
+        variables: { id }
+      })
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ title, id }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons" onClick={() => this.onSongDelete(id)}>
+            delete
+          </i>
         </li>
       );
     });
@@ -32,4 +43,12 @@ class SongList extends Component {
   }
 }
 
-export default graphql(fetchSongs)(SongList);
+const mutation = gql`
+  mutation deleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default compose(graphql(fetchSongs), graphql(mutation))(SongList);
