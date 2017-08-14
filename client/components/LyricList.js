4e8af692321c3198bf8cb0e4d18a8c2407480db5
Mali Michael <malimccalla@gmail.com>
2017-08-14 12:16:22 +0100

    Optimistic UI Response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import { graphql, gql } from 'react-apollo';
 
 class LyricList extends Component {
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
+    });
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id, likes)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
@@ -21,4 +44,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation likeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
